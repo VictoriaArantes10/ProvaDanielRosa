535cbc3c967c32172961c7481c3934a8006f7707
VictoriaArantes10 <victoria_amrosa@hotmail.com>
2022-10-04 19:43:59 -0300

    aaa

---
+++ ProvaRosa/Controllers/ClienteController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Prova.Data;
+using Prova.Models;
+
+namespace Prova.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private readonly AppCont _appCont;
+
+        public ClienteController(AppCont appCont)
+        {
+            _appCont = appCont;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id, Nome, Email, Cpf, DataNascimento")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _appCont.Add(cliente);
+                await _appCont.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(cliente);
+        }
+
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var cliente = await _appCont.Clientes.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (cliente == null)
+                return NotFound();
+
+            return View(cliente);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cliente = await _appCont.Clientes.FindAsync(id);
+            _appCont.Clientes.Remove(cliente);
+            await _appCont.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ ProvaRosa/Models/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Prova.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Email é obrigatório")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "CPF é obrigatório")]
+        public string Cpf { get; set; }
+
+        [Required(ErrorMessage = "Data de Nascimento é obrigatório")]
+        public DateTime DataNascimento { get; set; }
+    }
+}
